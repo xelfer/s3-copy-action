0f25b94fdacfa02035993f2aa35e6ce8b3fc7709
Nick Triantafillou <nicolast@gmail.com>
2023-07-05 12:17:22 +1000

    try 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,6 @@ if [ -z "$AWS_S3_BUCKET" ]; then
   exit 1
 fi
 
-if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-  exit 1
-fi
-
-if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-  exit 1
-fi
-
 # Default to us-east-1 if AWS_REGION not set.
 if [ -z "$AWS_REGION" ]; then
   AWS_REGION="us-east-1"
@@ -37,41 +27,8 @@ else
   DEST_PATH="s3://${AWS_S3_BUCKET}/${DEST_DIR}" # AWS S3 directory as destination
 fi
 
-AWS_PROFILE=s3-sync-action
-
-# Create a dedicated profile for this action to avoid conflicts
-# with past/future actions.
-aws configure --profile ${AWS_PROFILE} <<- EOF > /dev/null 2>&1
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
-${AWS_REGION}
-text
-EOF
-
-if [ -n "$AWS_ASSUME_ROLE_ARN" ]; then
-  echo "Assuming role: ${AWS_ASSUME_ROLE_ARN}"
-
-  # Create a profile to assume the role with.
-  {
-    echo "[profile s3-sync-action-assume]"
-    "role_arn = ${AWS_ASSUME_ROLE_ARN}"
-    "source_profile = ${AWS_PROFILE}"
-  } >> ~/.aws/config
-
-  AWS_PROFILE=s3-sync-action-assume
-fi
-
-# Sync using our dedicated profile and suppress verbose messages.
-# All other flags are optional via the `args:` directive.
 CMD_PREFIX="aws s3 sync"
-if [ -n "$AWS_S3_SSE_KMS_KEY_ID" ]; then
-  CMD_PREFIX="${CMD_PREFIX} --sse aws:kms --sse-kms-key-id ${AWS_S3_SSE_KMS_KEY_ID}"
-fi
-
-sh -c "${CMD_PREFIX} ${SOURCE_PATH} ${DEST_PATH} \
-  --profile ${AWS_PROFILE} \
-  --no-progress \
-  ${ENDPOINT_APPEND} $*"
+sh -c "${CMD_PREFIX} ${SOURCE_PATH} ${DEST_PATH} ${ENDPOINT_APPEND} $*"
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
